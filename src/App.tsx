a812b69eb9bd8879a5ebbf7e7fabdad47b815b3b
Ulugbek-Raxmatillayev <ulugbekraxmatillayev615@gmail.com>
2024-09-25 17:32:30 +0500

    update add query

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { Route, Routes } from "react-router-dom"
 import Login from "./pages/auth/login"
 import Dashboard from "./pages/dashboard/dashboard"
-import { createContext } from "vm"
+import { QueryClient, QueryClientProvider } from "react-query"
 
+const queryClient = new QueryClient()
 function App(): JSX.Element {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Routes>
         <Route path="/login" element={<Login/>}/>
         <Route path="/admin/dashboard" element={<Dashboard/>}/>
       </Routes>
-    </>
+    </QueryClientProvider>
   )
 }
 
